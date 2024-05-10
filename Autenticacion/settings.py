11e8d7c071532f261ef9c2ee3757e08a00c4b8a2
Feder1414 <f.bogota@uniandes.edu.co>
2024-05-10 15:49:47 -0500

    asdfsadfasdfsadfasd

--- Autenticacion/settings.py
+++ Autenticacion/settings.py
@@ -85,6 +85,16 @@ WSGI_APPLICATION = 'Autenticacion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'basealpes',
+#         'USER': 'usuario',
+#         'PASSWORD': 'pepe',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
